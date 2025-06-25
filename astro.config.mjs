ac45ca5be5808c22a8b3f571d05a3ad26fe17f82
alessio-its <alessio.pinto.14-2@accademiadigitaleliguria.it>
2025-06-25 09:53:37 +0200

    Modified config for astro web analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,14 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 
-import vercel from "@astrojs/vercel";
+import vercel from '@astrojs/vercel/serverless'; 
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   adapter: vercel({
     webAnalytics: {
-      enabled: true,
+      enabled: false,
     },
   }),
 });
